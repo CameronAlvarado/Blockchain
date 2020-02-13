de7182b270351efc9f33fd56d970b05be1713610
Cameron Alvarado <cameronalvarado@Camerons-Macintosh.local>
2020-02-13 14:17:00 -0800

    created shell for ap

---
+++ basic_wallet_p/wallet_app/src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./App.css";
+import Main from "./components/Main.js";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Wallet App</h1>
+      </header>
+      <Main />
+    </div>
+  );
+}
+
+export default App;
---
+++ basic_wallet_p/wallet_app/src/components/Main.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "../App.css";
+
+function Main() {
+  return (
+    <div className="Main">
+      <h2>Id</h2>
+
+      <h2>Balance</h2>
+
+      <h2>Transactions</h2>
+    </div>
+  );
+}
+
+export default Main;
